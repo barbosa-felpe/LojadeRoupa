29817a9b66abaf630ebecd6dfdd3741561a8b70e
FELIPE BARBOSA <felipe_barbosa153@estudante.sesisenai.org.br>
2025-11-04 17:10:24 -0300

    novas alterações no index

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: true,
+      freeMode: false,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
