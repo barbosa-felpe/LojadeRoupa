11f2d7f5b8674bea5f52ea45b0932435fcbf537a
FELIPE BARBOSA <felipe_barbosa153@estudante.sesisenai.org.br>
2025-12-08 17:09:50 -0300

    login page

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        senha = request.form["senha"]
+        
+        return redirect("/")
+    return render_template("login.html")
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
